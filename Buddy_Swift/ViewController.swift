4ac10b0982f68c340d9256a35a9ddb11c446df8d
Atisha Poojary <Atisha@atishas-mbp.fios-router.home>
2016-05-17 00:06:10 -0400

    Initial Commit

---
+++ Buddy_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buddy_Swift
+//
+//  Created by Atisha Poojary on 17/05/16.
+//  Copyright © 2016 Atisha Poojary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
